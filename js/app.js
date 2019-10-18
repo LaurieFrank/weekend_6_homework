ff3dac474a34297bffd914be89b8490ceced970a
Laurie Frank <laurie360@hotmail.co.uk>
2019-10-18 14:29:32 +0100

    mvp complete

--- js/app.js
+++ js/app.js
@@ -3,26 +3,38 @@ document.addEventListener('DOMContentLoaded', () => {
 
   const form = document.querySelector("#new-artist-form");
   form.addEventListener("submit", handleFormSubmit);
+
+  const deleteAllButton = document.querySelector('#delete-all');
+  deleteAllButton.addEventListener('click', handleDeleteAllClick)
 })
 
   const handleFormSubmit = function (event) {
     event.preventDefault();
     const resultFormSubmit = document.querySelector("#artist-list");
 
-    const newSubmission = document.createElement("ul");
+    const newSubmission = document.createElement("p");
     resultFormSubmit.appendChild(newSubmission);
 
-    const newArtist = document.createElement("li");
+    const newArtist = document.createElement("h2");
     newArtist.textContent = event.target.artist.value;
     newSubmission.appendChild(newArtist);
 
-    const newGenre = document.createElement("li");
+    const newGenre = document.createElement("p");
     newGenre.textContent = event.target.genre.value;
     newSubmission.appendChild(newGenre);
 
-    const newBestAlbum = document.createElement("li");
+    const newBestAlbum = document.createElement("h2");
     newBestAlbum.textContent = event.target.best_album.value;
     newSubmission.appendChild(newBestAlbum);
 
     event.target.reset()
+
+    return resultFormSubmit;
+
+
+  }
+
+  const handleDeleteAllClick = function(event){
+    const resultFormSubmit = document.querySelector('#artist-list');
+    resultFormSubmit.innerHTML = '';
   }
