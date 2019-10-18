c31712d6cf2e8ab284991069d9a2f136af4b5d1b
Laurie Frank <laurie360@hotmail.co.uk>
2019-10-18 13:50:40 +0100

    completed js form submission code

--- js/app.js
+++ js/app.js
@@ -12,5 +12,17 @@ document.addEventListener('DOMContentLoaded', () => {
     const newSubmission = document.createElement("ul");
     resultFormSubmit.appendChild(newSubmission);
 
-    const new
+    const newArtist = document.createElement("li");
+    newArtist.textContent = event.target.artist.value;
+    newSubmission.appendChild(newArtist);
+
+    const newGenre = document.createElement("li");
+    newGenre.textContent = event.target.genre.value;
+    newSubmission.appendChild(newGenre);
+
+    const newBestAlbum = document.createElement("li");
+    newBestAlbum.textContent = event.target.best_album.value;
+    newSubmission.appendChild(newBestAlbum);
+
+    event.target.reset()
   }
